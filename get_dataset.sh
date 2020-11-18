97f0cf2acb05d42e22893a066bb8268d2007722a
jdice <siddalljosh@gmail.com>
2020-11-17 21:44:34 -0500

    typo fix

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,7 +1,7 @@
-lsRoot=http://www.cs.jhu.edu/~qiuwch/animal/
+Root=http://www.cs.jhu.edu/~qiuwch/animal/
 mkdir -p animal_data/synthetic_animal
-wget -c $Root/synthetic_animal/horse_combineds5r5_texture.zip -O animal_data/synthetic_animal/horse_combineds5r5_texture.zip
-wget -c $Root/synthetic_animal/tiger_combineds5r5_texture.zip -O animal_data/synthetic_animal/tiger_combineds5r5_texture.zip
+wget -c http://www.cs.jhu.edu/~qiuwch/animal/synthetic_animal/horse_combineds5r5_texture.zip -O animal_data/synthetic_animal/horse_combineds5r5_texture.zip
+wget -c http://www.cs.jhu.edu/~qiuwch/animal/synthetic_animal/tiger_combineds5r5_texture.zip -O animal_data/synthetic_animal/tiger_combineds5r5_texture.zip
 #wget -c $Root/synthetic_animal/sheep_combineds5r5_texture.zip -O animal_data/synthetic_animal/sheep_combineds5r5_texture.zip
 #wget -c $Root/synthetic_animal/hound_combineds5r5_texture.zip -O animal_data/synthetic_animal/hound_combineds5r5_texture.zip
 #wget -c $Root/synthetic_animal/elephant_combineds5r5_texture.zip -O animal_data/synthetic_animal/elephant_combineds5r5_texture.zip
